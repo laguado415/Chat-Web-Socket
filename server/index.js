96032779a519d5d9e65b40627760cc1313195bbf
laguado415 <jonathanlaguado99@gmail.com>
2024-05-28 03:34:05 -0500

    build: project initialization
    project initialization

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import http from "http";
+import { Server as SocketServer } from "socket.io";
+
+// inicializacion del server
+const app = express();
+
+// se crea servidor http
+const server = http.createServer(app);
+
+// se establece en el server de scket.io
+const io = new SocketServer(server);
+
+server.listen(3001);
+console.log("server on port", 3001);
